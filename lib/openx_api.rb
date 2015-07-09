51144c603f9af8a45f3c758f45c88e5b363fb942
infinite-monkeys <infinite-monkeys@users.noreply.github.com>
2015-07-09 14:36:37 -0700

    Move OX3APIClient into OpenxApi namespace

--- lib/openx_api.rb
+++ lib/openx_api.rb
@@ -1,165 +1,169 @@
+require "openx_api/version"
+
 require 'oauth'
 require 'yaml'
 
-class OX3APIClient < OAuth::Consumer
+module OpenxApi
+  class OX3APIClient < ::OAuth::Consumer
 
-  def initialize(email, password, site_url, consumer_key, consumer_secret, realm,
-    version='v2', sso_domain='sso.openx.com', callback='oob', scheme='https', debug=false)
+    def initialize(email, password, site_url, consumer_key, consumer_secret, realm,
+      version='v2', sso_domain='sso.openx.com', callback='oob', scheme='https', debug=false)
 
-    @version, @callback, @site, @debug = version, callback, site_url, debug
-    @site = @site.end_with?('/') ? @site.chop : @site
+      @version, @callback, @site, @debug = version, callback, site_url, debug
+      @site = @site.end_with?('/') ? @site.chop : @site
 
-    super(consumer_key, consumer_secret, {
-      :http_method => :post,
-      :scheme => :header,
-      :oauth_version => "1.0",
-      :signature_method => "HMAC-SHA1",
-      :site => @site,
-      :request_token_url => scheme + '://' + sso_domain + '/api/index/initiate',
-      :access_token_url => scheme + '://' + sso_domain + '/api/index/token',
-      :authorize_path => scheme + '://' + sso_domain + '/login/process',
-      :realm => realm
-    })
+      super(consumer_key, consumer_secret, {
+        :http_method => :post,
+        :scheme => :header,
+        :oauth_version => "1.0",
+        :signature_method => "HMAC-SHA1",
+        :site => @site,
+        :request_token_url => scheme + '://' + sso_domain + '/api/index/initiate',
+        :access_token_url => scheme + '://' + sso_domain + '/api/index/token',
+        :authorize_path => scheme + '://' + sso_domain + '/login/process',
+        :realm => realm
+      })
 
-    # Step 1. Fetch temporary request token.
-    fetch_request_token
+      # Step 1. Fetch temporary request token.
+      fetch_request_token
 
-    # Step 2. Log in to SSO server and authorize token.
-    authorize_token(email, password)
+      # Step 2. Log in to SSO server and authorize token.
+      authorize_token(email, password)
 
-    # Step 3. Swap temporary request token for permanent access token.
-    fetch_access_token
+      # Step 3. Swap temporary request token for permanent access token.
+      fetch_access_token
 
-    # Step 4. Validate your access token.
-    validate_session
-  end
+      # Step 4. Validate your access token.
+      validate_session
+    end
 
-################################################################################################################
-  def perform_request
-    http = self.create_http(@site)
-    http.open_timeout =  5 * 60
-    http.read_timeout = 15 * 60
-    params = Hash.new
-    params['Content-Type'] = 'application/x-www-form-urlencoded'
-    params['Cookie'] = 'openx3_access_token=' + @acccess_token.token + '; domain=' + get_domain + '; path=/'
-    prefix = @version == 'v1' ? "/ox/3.0" : "/ox/4.0"
-    response = http.request yield prefix, params
-    response.body
-  end
+  ################################################################################################################
+    def perform_request
+      http = self.create_http(@site)
+      http.open_timeout =  5 * 60
+      http.read_timeout = 15 * 60
+      params = Hash.new
+      params['Content-Type'] = 'application/x-www-form-urlencoded'
+      params['Cookie'] = 'openx3_access_token=' + @acccess_token.token + '; domain=' + get_domain + '; path=/'
+      prefix = @version == 'v1' ? "/ox/3.0" : "/ox/4.0"
+      response = http.request yield prefix, params
+      response.body
+    end
 
-  def get(path)
-    perform_request do |prefix, params|
-      self.create_http_request(
-        :get,
-        prefix + path,
-        params
+    def get(path)
+      perform_request do |prefix, params|
+        self.create_http_request(
+          :get,
+          prefix + path,
+          params
+        )
+      end
+    end
+
+    def post(path, body = {})
+      perform_request do |prefix, params|
+        self.create_http_request(
+          :post,
+          prefix + path,
+          body,
+          params
+        )
+      end
+    end
+
+    def delete(path)
+      perform_request do |prefix, params|
+        self.create_http_request(
+          :delete,
+          prefix + path,
+          params
+        )
+      end
+    end
+
+    def logoff
+      delete "/a/session"
+    end
+  ################################################################################################################
+
+  private
+    def fetch_request_token
+      @request_token = self.get_request_token(
+        {:oauth_callback => @callback},
+        {'Content-Type' => 'application/x-www-form-urlencoded'}
       )
+      if @debug
+        puts YAML::dump(@request_token)
+      end
     end
-  end
 
-  def post(path, body = {})
-    perform_request do |prefix, params|
-      self.create_http_request(
-        :post,
-        prefix + path,
-        body,
-        params
+    def authorize_token(email, password)
+      authorize = self.request(:post,  self.authorize_path, nil,
+        {:oauth_token => @request_token.token, :oauth_callback => @callback},
+        {:email => email, :password => password, :oauth_token => @request_token.token},
+        {'Content-Type' => 'application/x-www-form-urlencoded'}
       )
+      if authorize.code.to_s == '200'
+        response = authorize
+      else
+        response = self.request(:post, authorize.header['Location'], nil,
+          {:oauth_token => @request_token.token, :oauth_callback => @callback}, nil,
+          {
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Cookie' => authorize.get_fields('set-cookie')[0],
+          }
+        )
+      end
+      if @debug
+        puts YAML::dump(response)
+      end
+      @oauth_verifier = parse_tokens(response.body)[:oauth_verifier]
     end
-  end
 
-  def delete(path)
-    perform_request do |prefix, params|
-      self.create_http_request(
-        :delete,
-        prefix + path,
-        params
+    def fetch_access_token
+      @acccess_token = @request_token.get_access_token(
+        {:oauth_verifier => @oauth_verifier, :oauth_token => @request_token.token, :oauth_callback => @callback},
+        {'Content-Type' => 'application/x-www-form-urlencoded'}
       )
+      if @debug
+        puts YAML::dump(@acccess_token)
+      end
     end
-  end
 
-  def logoff
-    delete "/a/session"
-  end
-################################################################################################################
-
-private
-  def fetch_request_token
-    @request_token = self.get_request_token(
-      {:oauth_callback => @callback},
-      {'Content-Type' => 'application/x-www-form-urlencoded'}
-    )
-    if @debug
-      puts YAML::dump(@request_token)
-    end
-  end
-
-  def authorize_token(email, password)
-    authorize = self.request(:post,  self.authorize_path, nil,
-      {:oauth_token => @request_token.token, :oauth_callback => @callback},
-      {:email => email, :password => password, :oauth_token => @request_token.token},
-      {'Content-Type' => 'application/x-www-form-urlencoded'}
-    )
-    if authorize.code.to_s == '200'
-      response = authorize
-    else
-      response = self.request(:post, authorize.header['Location'], nil,
-        {:oauth_token => @request_token.token, :oauth_callback => @callback}, nil,
+    def validate_session
+=begin
+      path = @version == 'v1' ? "/ox/3.0/a/session/validate" : "/ox/4.0/session"
+      method = @version == 'v1' ? :put : :get
+      response = self.request(method.to_sym, @site + path, nil, {}, nil,
         {
           'Content-Type' => 'application/x-www-form-urlencoded',
-          'Cookie' => authorize.get_fields('set-cookie')[0],
+          'Cookie' => 'openx3_access_token=' + @acccess_token.token + '; domain=' + get_domain + '; path=/'
         }
       )
-    end
-    if @debug
-      puts YAML::dump(response)
-    end
-    @oauth_verifier = parse_tokens(response.body)[:oauth_verifier]
-  end
-
-  def fetch_access_token
-    @acccess_token = @request_token.get_access_token(
-      {:oauth_verifier => @oauth_verifier, :oauth_token => @request_token.token, :oauth_callback => @callback},
-      {'Content-Type' => 'application/x-www-form-urlencoded'}
-    )
-    if @debug
-      puts YAML::dump(@acccess_token)
-    end
-  end
-
-  def validate_session
-=begin
-    path = @version == 'v1' ? "/ox/3.0/a/session/validate" : "/ox/4.0/session"
-    method = @version == 'v1' ? :put : :get
-    response = self.request(method.to_sym, @site + path, nil, {}, nil,
-      {
-        'Content-Type' => 'application/x-www-form-urlencoded',
-        'Cookie' => 'openx3_access_token=' + @acccess_token.token + '; domain=' + get_domain + '; path=/'
-      }
-    )
 =end
-    response = perform_request do |prefix, params|
-      self.create_http_request(
-        @version == 'v1' ? :put : :get,
-        prefix + (@version == 'v1' ? "/a/session/validate" : "/session"),
-        nil,
-        params
-      )
+      response = perform_request do |prefix, params|
+        self.create_http_request(
+          @version == 'v1' ? :put : :get,
+          prefix + (@version == 'v1' ? "/a/session/validate" : "/session"),
+          nil,
+          params
+        )
+      end
+      if @debug
+        puts YAML::dump(response)
+      end
     end
-    if @debug
-      puts YAML::dump(response)
+
+    def parse_tokens(keys)
+      keys.split("&").inject({}) do |hash, pair|
+        key, value = pair.split("=")
+        hash.merge({ key.to_sym => CGI.unescape(value) })
+      end
     end
-  end
 
-  def parse_tokens(keys)
-    keys.split("&").inject({}) do |hash, pair|
-      key, value = pair.split("=")
-      hash.merge({ key.to_sym => CGI.unescape(value) })
+    def get_domain
+      URI.parse(@site).host
     end
-  end
 
-  def get_domain
-    URI.parse(@site).host
   end
-
 end
--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'json'
 require 'oauth'
-require_relative 'ox3client.rb'
+require 'openx_api'
 
 
 email = ''
@@ -11,7 +11,7 @@ consumer_secret = ''
 realm = ''
 site_url = ''
 
-ox3 = OX3APIClient.new(email, password, site_url, consumer_key, consumer_secret, realm)
+ox3 = OpenxApi::OX3APIClient.new(email, password, site_url, consumer_key, consumer_secret, realm)
 puts ox3.get('/a/site')
 
 site = ox3.post('/a/site/12345',
@@ -26,11 +26,11 @@ puts site['name']
 ##################
 #  OpenX API v1
 ##################
-ox3v1 = OX3APIClient.new(email, password, site_url, consumer_key, consumer_secret, realm, 'v1')
+ox3v1 = OpenxApi::OX3APIClient.new(email, password, site_url, consumer_key, consumer_secret, realm, 'v1')
 puts ox3v1.get('/a/account')
 
 ##################
 #  OpenX API v2
 ##################
-ox3v2 = OX3APIClient.new(email, password, site_url, consumer_key, consumer_secret, realm, 'v2')
+ox3v2 = OpenxApi::OX3APIClient.new(email, password, site_url, consumer_key, consumer_secret, realm, 'v2')
 puts JSON.parse(ox3v2.get('/account'))['objects']
