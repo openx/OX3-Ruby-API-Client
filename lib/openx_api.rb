5e4f534af9af5c7da14aa6072c21a871f4e3ad73
infinite-monkeys <infinite-monkeys@users.noreply.github.com>
2015-07-09 14:17:24 -0700

    Add basic gem stuff

---
+++ lib/openx_api.rb
@@ -0,0 +1,5 @@
+require "openx_api/version"
+
+module OpenxApi
+  # Your code goes here...
+end
---
+++ spec/openx_api_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe OpenxApi do
+  it 'has a version number' do
+    expect(OpenxApi::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
